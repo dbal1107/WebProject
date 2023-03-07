061e1d105303d07d4826a6ce3fc3671c2b6fd0ca
jjyumi <jjpenta26@gmail.com>
2023-03-07 20:12:54 +0900

    -11

--- codeit/index.js
+++ codeit/index.js
@@ -381,3 +381,227 @@
 //   current = current + temp;  // temp에는 기존 previous 값이 저장돼 있음
 // }
 
+
+
+
+
+
+
+
+
+// let myVoca = {
+//     function : "함수",
+//     variable : "변수",
+//     constant : "상수",
+//     local : "지역의",
+//     global : "전반적인",
+// };
+
+// // console.log(myVoca);
+// // console.log(myVoca.local);
+// // console.log(myVoca.constant);
+// // console.log(myVoca.function);
+
+// delete myVoca.function;
+// delete myVoca.local;
+// delete myVoca.constant;
+
+// console.log(myVoca);
+// console.log(myVoca.export);
+
+// myVoca.extend = "확장하다";
+// myVoca.export = "내보내다";
+// myVoca.import = "불러오다";
+// myVoca["default value"] = "기본값";
+
+// console.log(myVoca);
+// console.log(myVoca.export);
+
+// console.log(["default value"]);
+
+
+
+
+// let greeting ={
+//     sayHello: function(name){
+//         console.log(`Hello ${name}`);
+//     }
+// }
+
+// greeting.sayHello("이름");
+
+
+
+
+
+// let myVoca = {
+//     addVoca: function(voca, mean){
+//         myVoca[voca] = mean;
+//     },
+//     deleteVoca: function(voca){
+//         delete myVoca[voca];
+//     },
+//     printVoca: function(voca){
+//         console.log(`"${voca}"의 뜻은 "${myVoca[voca]}"입니다.`);
+//     },
+// };
+
+// // addVoca메소드 테스트 코드
+// myVoca.addVoca('parameter', '매개 변수');
+// myVoca.addVoca('element', '요소');
+// myVoca.addVoca('property', '속성');
+// console.log(myVoca);
+
+// // deleteVoca메소드 테스트 코드
+// myVoca.deleteVoca('parameter');
+// myVoca.deleteVoca('element');
+// console.log(myVoca);
+
+// // printVoca메소드 테스트 코드
+// myVoca.printVoca('property');
+
+
+
+
+
+
+
+
+// let codeit = {
+//     name: '코드잇',
+//     bornYear: 2017,
+//     isVeryNice: true,
+//     worstCourse: null,
+//     bestCourse: '자바스크립트 프로그래밍 기초'
+// }
+
+// for (let key in codeit) {
+//     console.log(key);
+//     console.log(codeit[key]);
+// }
+
+
+
+
+
+
+// let hyesoonScore = {
+// 	'데이터 모델링의 이해': 10,
+// 	'데이터 모델과 성능': 8,
+// 	'SQL 기본': 22,
+// 	'SQL 활용': 18,
+// 	'SQL 최적화 기본 원리': 20,
+// };
+
+// let minsoonScore = {
+// 	'데이터 모델링의 이해': 14,
+// 	'데이터 모델과 성능': 8,
+// 	'SQL 기본': 12,
+// 	'SQL 활용': 4,
+// 	'SQL 최적화 기본 원리': 16,
+// };
+
+// function passChecker(scoreObject) {
+//     let sum = 0;
+//     for (let subject in scoreObject){
+//         sum += scoreObject[subject];
+//     }
+//     if (sum >= 60) {
+//         console.log("축하합니다! 합격입니다!");
+//     } else {
+//         console.log("아쉽지만 불합격입니다..")
+//     }
+// }
+
+// passChecker(hyesoonScore);
+// passChecker(minsoonScore);
+
+
+
+
+
+
+// let today = new Date(2112, 8, 24);
+// let jaeSangStart = new Date(2109, 7, 1);
+
+// function workDayCalc(startDate) {
+// 	// 여기에 코드를 작성하세요
+//     let date = ((today-jaeSangStart)/1000/86400)+1;
+// 	console.log(`오늘은 입사한 지 ${date}일째 되는 날 입니다.`);
+// }
+
+// workDayCalc(jaeSangStart);
+
+
+
+
+
+
+
+
+// let fruits = ['레몬', '토마토', '딸기', '바나나'];
+// let ages = [20, 24, 25, 29, 30, 33];
+// let numbers = [];
+
+// // fruits 배열에 '토마토'를 삭제하고 그 자리에 '사과', '청포도' 를 추가해 주세요
+// fruits.splice(1,1,'사과','청포도');
+// console.log(fruits);
+
+// // fruits 배열의 첫 번째 요소를 삭제해 주세요
+// fruits.splice(0,1);
+
+// // ages 배열에 마지막 요소를 삭제해 주세요
+// ages.splice(ages.length-1,1);
+
+// // ages 배열의 2번, 3번 인덱스를 26, 28로 변경해 주세요
+// ages.splice(2,2,26,28);
+
+// // numbers 배열에 1, 2, 3, 5, 8, 9를 순서대로 추가해 주세요
+// numbers = [1,2,3,5,8,9];
+
+// // 반복문을 활용해서 numbers 배열의 요소들 중 홀수를 모두 삭제해 주세요
+// for (let i=(numbers.length-1); i>=0; i--){
+//     if (((numbers[i])%2) !== 0) {
+//         numbers.splice(i, 1);
+//     }
+// }
+
+// // 테스트 코드
+// console.log(fruits[1]);
+// console.log(fruits[0]);
+// console.log(ages[ages.length - 1]);
+// console.log(ages[3]);
+// console.log(numbers[3]);
+// console.log(numbers);
+
+
+
+
+
+
+
+
+// 투표 결과 리스트
+let votes = [
+    '이재식', '이재식', '이규하', '이규하', '이규하',
+    '이재식', '이재식', '이규하', '이규하', '이재식',
+    '이규하', '이규하', '이규하', '이규하', '이재식',
+    '이재식', '이규하', '이재식', '이재식', '이재식',
+    '이재식', '이재식', '이규하', '이규하', '이규하',
+    '이규하', '이규하', '이재식', '이규하', '이규하',
+    '이규하', '이규하', '이재식', '이규하', '이규하',
+    '이규하', '이재식', '이재식', '이재식', '이규하',
+  ];
+  
+  // 후보별 득표수 객체
+  let voteCounter = {};
+  
+  // votes 배열을 이용해서 voteCounter 객체를 정리하기
+  for (let name of votes) {
+    // 여기에 코드를 작성하세요
+    // votes의 name을 불러와서 더하기.
+    let person1 = {}
+  }
+  
+  // 후보별 득표수 출력
+  console.log(voteCounter);
